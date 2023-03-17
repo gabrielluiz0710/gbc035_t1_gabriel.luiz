39ea977eb40904acdf3d5cdcee126e9cab741f31
Gabriel Luiz <gabriel.luiz@ufu.br>
2023-03-16 23:56:17 -0300

    Resposta questão 1

---
+++ LAB01 - Procedural/ex01/HelloWorld.java
@@ -0,0 +1,10 @@
+class HelloWorld
+{
+	public static void main (String[] args)
+	{
+
+		System.out.println ("Hello World");
+		System.out.println ("Nome: Gabriel Luiz de Lima Soares");
+		System.out.println ("Matrícula: 12121BCC042");
+	}
+}
